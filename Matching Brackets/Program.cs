7e75ef57791a7f8ae918a4a522e0811999c492e6
Ren4ik <ren.zigan@gmail.com>
2018-01-27 15:10:34 +0300

    Завершено

--- Matching Brackets/Program.cs
+++ Matching Brackets/Program.cs
@@ -7,19 +7,20 @@ namespace Matching_Brackets
     {
         public static void Main(string[] args)
         {
+            DoWork d = new DoWork();
+            d.StartProgram();
         }
     }
 
     class DoWork
     {
         private const string Brackets = "(){}[]<>";
-        ptivate const string BracketsBegan = "({[<";
+        private const string BracketsBegan = "({[<";
         private int plus;
         private int countOfTests;
         private string input;
         private List<char> testList;
-        private List<char> tempTestList;
-        ptivate int status, indexEnd;
+        private int status, indexEnd;
 
         public void StartProgram()
         {
@@ -29,7 +30,7 @@ namespace Matching_Brackets
             {
                 input = Console.ReadLine();
                 deleteOrAddToList();
-                status = (testList % 2 == 0) ? checkTestList() : 0;
+                status = (testList.Count % 2 == 0) ? checkTestList() : 0;
                 Console.Write("{0} ", status);
             }
         }
@@ -37,7 +38,6 @@ namespace Matching_Brackets
         void deleteOrAddToList()
         {
         	testList = new List<char>();
-        	tempTestList = new List<char>();
             foreach (var c in input)
             {
                 if (Brackets.Contains(c.ToString()))
@@ -47,20 +47,41 @@ namespace Matching_Brackets
 
         int checkTestList()
         {
+            int testingNum = 0;
+
         	for(int i = 0; i < testList.Count; i++)
         	{
         		if( !BracketsBegan.Contains( testList[i].ToString() ) )
         		   continue;
         		plus = testList[i].Equals('(') ? 1 : 2;
-        		char temp = Convert.ToChar(Convert.ToUInt32(testList[0]) + plus);
+        		char temp = Convert.ToChar(Convert.ToUInt32(testList[i]) + plus);
         		indexEnd = testList.IndexOf(temp, i);
-        		if (indexEnd == -1) && ( (indexEnd - i + 1) % 2 != 0) )
+                testingNum = i + 1;
+                while ( isThereBracketInBracket(ref testingNum, testList[i]) )
+                {
+                    indexEnd = testList.IndexOf(temp, indexEnd+1);
+                    testingNum++;
+                }
+        		if ( (indexEnd == -1) || (indexEnd - i + 1) % 2 != 0) 
         		{
         			return 0;
         		}
         	}
             
             return 1;
+        }
+
+        private bool isThereBracketInBracket(ref int testingNum, char temp)
+        {
+            for (; testingNum < indexEnd; testingNum++)
+            {
+                if (testList[testingNum].Equals(temp))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
